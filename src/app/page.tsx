a0d23bba2b54dd3c126a329df35f6160d6d8d902
Fran <fm-a@robindatahood.com>
2024-09-06 12:40:35 +0200

    tailwind

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='p-4 bg-green-400 text-xl text-green-200'>
+        Hello world!
+        </div>
+  )
+}
