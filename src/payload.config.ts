436d22aa913e89417ba9a98b7b70a141aef0da4f
Fran <fm-a@robindatahood.com>
2024-09-06 11:00:03 +0200

    first commit

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -8,6 +8,7 @@ import sharp from 'sharp'
 
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
+import { s3Storage } from '@payloadcms/storage-s3'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -30,6 +31,27 @@ export default buildConfig({
   }),
   sharp,
   plugins: [
-    // storage-adapter-placeholder
+    s3Storage({
+      collections: {
+        'media': {
+          prefix: 'media',
+        }
+
+      },
+      //@ts-ignore
+      bucket: process.env.S3_BUCKET,
+      config: {
+        credentials: {
+          //@ts-ignore
+          accessKeyId: process.env.S3_ACCESS_KEY_ID,
+          //@ts-ignore
+          secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+        },
+        region: process.env.S3_REGION || 'eu-north-1',
+        endpoint: process.env.S3_ENDPOINT,
+       forcePathStyle: true,
+      },
+    }),
+
   ],
 })
